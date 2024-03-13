4ba9fb4c07b82aaa3bc3018bb409530a27c558c1
Ayushman Bhattacharya <74301576+Circuit-Overtime@users.noreply.github.com>
2024-03-13 11:14:09 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    app: {
+        token: 'your bot token goes here',
+        playing: 'by Resonance ❤️',
+        global: false,
+        guild: 'your server ID goes here',
+        ExtraMessages: false,
+        loopMessage: false,
+
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: '',
+            commands: []
+        },
+        maxVol: 100,
+        spotifyBridge: true,
+        volume: 75,
+        leaveOnEmpty: false,
+        leaveOnEmptyCooldown: 30000,
+        leaveOnEnd: true,
+        leaveOnEndCooldown: 30000,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+};
